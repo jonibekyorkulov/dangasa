946743880c618f33852be344561bc6f8bbe2105e
jonibekyorkulov <jonibekyorkulov@gmail.com>
2024-03-10 15:27:35 -0400

    Create Class

---
+++ dangasa/meneger/model_manager.py
@@ -0,0 +1,21 @@
+class ModelManager:
+    def __init__(self) -> None:
+        pass
+    
+    def create_views(self):
+        pass
+    
+    def create_serializers(self):
+        pass
+    
+    def create_urls(self):
+        pass
+    
+    def create_tests(self):
+        pass
+    
+    def main(self):
+        self.create_views()
+        self.create_serializers()
+        self.create_urls()
+        self.create_tests()
