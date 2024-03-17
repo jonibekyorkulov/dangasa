c8936229c4798c6130231664f460bbea0212915c
jonibekyorkulov <jonibekyorkulov@gmail.com>
2024-03-17 01:39:40 -0400

    view serializers urls update

--- dangasa/manager/model_manager.py
+++ dangasa/manager/model_manager.py
@@ -74,13 +74,9 @@ class ModelManager:
             f'\nfrom .serializers import {self.model_name}Serializer',
         ]
         
-        # new_models_to_add = {self.model_name}
-        # new_serializers_to_add = f'{self.model_name}Serializer'
 
         import_pattern = re.compile(r'import\s+(.*?)(?=\s|from|$)', re.MULTILINE)
 
-        # models_import_pattern = re.compile(r'from\s+\.\w+\s+import\s+', re.DOTALL)
-        # serializers_import_pattern = re.compile(r'from\s+\.\w+\s+import\s+', re.DOTALL)
 
         match = import_pattern.search(content)
         new_content = str()
@@ -93,7 +89,7 @@ class ModelManager:
             for module_to_import in modules_to_import:
                 if module_to_import not in content:
                     new_content += module_to_import + '\n'
-            new_content += '\n' + content[end_index:]
+            new_content += content[end_index:]
 
         else:
             for module_to_import in modules_to_import:
@@ -162,8 +158,30 @@ class ModelManager:
             for key, value in replacement_dict.items():
                 content = content.replace(key, value)
                 
+            if f'{self.model_name}Serializer' not in content:   
+                current_dir = os.path.dirname(os.path.abspath(__file__))
+                source_file_list_create = os.path.join(current_dir, 'model_crud_files/serializers_class.py-tpl')
+                
+                new_text_file_list_create = Path(source_file_list_create)
+                content_list_create = new_text_file_list_create.read_text()
+
+                replacement_dict = {
+                    '{model_name}': self.model_name,  
+                    '{fields}': '( '+', '.join([f"'{field[0]}'" for field in self.fields]) + ', )',
+                }
+                
+                for key, value in replacement_dict.items():
+                    content_list_create = content_list_create.replace(key, value)
+                    
+                
+                
+                print(f"Serializer.py  '{self.app_name}' update successfully.")
+            else:
+                print(f"Serializer.py  < {self.model_name}Serializer >  already exists.")
+            
+            
             new_py_file = Path(self.app_name+'/serializers.py')
-            new_py_file.write_text(content)
+            new_py_file.write_text(content + '\n' + content_list_create)
             
             print(f"serializers.py '{self.app_name}' created successfully.")
         except FileNotFoundError:
@@ -174,7 +192,66 @@ class ModelManager:
             print(f"Xatolik yuz berdi: {e}")
             
     def add_serializers_file(self):
-        print('add_serializers_file')
+        old_py_file = Path(self.app_name+'/serializers.py')
+        content = old_py_file.read_text()
+        modules_to_import = [
+            f'\nfrom .models import {self.model_name}',
+        ]
+        
+        if "from rest_framework import serializers" not in content:
+            modules_to_import.append("from rest_framework import serializers")
+        
+        import_pattern = re.compile(r'import\s+(.*?)(?=\s|from|$)', re.MULTILINE)
+
+
+        match = import_pattern.search(content)
+        new_content = str()
+        if match:
+            # Izlash natijasini topish
+            end_index = match.end()
+
+            # Modullarni qo'shish
+            new_content = content[:end_index]
+            for module_to_import in modules_to_import:
+                if module_to_import not in content:
+                    new_content += module_to_import + '\n'
+            new_content += content[end_index:]
+
+        else:
+            for module_to_import in modules_to_import:
+                if module_to_import not in content:
+                    new_content += module_to_import + '\n'
+            print("Import statement not found in the file.")
+        
+        content_list_create = str()
+        if f'{self.model_name}Serializer' not in content:   
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            source_file_list_create = os.path.join(current_dir, 'model_crud_files/serializers_class.py-tpl')
+            
+            new_text_file_list_create = Path(source_file_list_create)
+            content_list_create = new_text_file_list_create.read_text()
+
+            replacement_dict = {
+                '{model_name}': self.model_name,  
+                '{fields}': '( '+', '.join([f"'{field[0]}'" for field in self.fields]) + ', )',
+            }
+            
+            for key, value in replacement_dict.items():
+                content_list_create = content_list_create.replace(key, value)
+                
+            
+            
+            print(f"Serializer.py  '{self.app_name}' update successfully.")
+        else:
+            print(f"Serializer.py  < {self.model_name}Serializer >  already exists.")
+        
+        
+        new_py_file = Path(self.app_name+'/serializers.py')
+        new_py_file.write_text(new_content + '\n' + content_list_create)
+        
+        
+        
+        
     
     def create_urls(self):
         try:
@@ -195,25 +272,25 @@ class ModelManager:
         content = old_py_file.read_text()
         new_data_to_add = f"path('{self.model_name}/list/', {self.model_name}ListCreateApiView.as_view()),\n"
         new_views_to_add = f'{self.model_name}ListCreateApiView,'
-        if new_data_to_add not in content:
+        if new_data_to_add not in content and new_data_to_add not in content:
             pattern = re.compile(r'urlpatterns\s*=\s*\[\s*', re.DOTALL)
             import_pattern = re.compile(r'from\s+\.views\s+import\s+', re.DOTALL)
-
-
             
             match = pattern.search(content)
             match_import = import_pattern.search(content)
-
-            if match and match_import:
+            
+            if match and match_import :
                 # Izlash natijasini topish
                 start_index = match.end()
                 
-                end_index_import = match_import.end(1)
+                end_index_import = match_import.end()
 
                 # Malumotni qo'shish
                 
                 new_content = content[:start_index] + new_data_to_add + '\n' + content[start_index:]
-                new_content_import = new_content[:end_index_import - 1] + new_views_to_add + '\n' + new_content[end_index_import:]
+                
+                new_content_import = new_content[:end_index_import ] + new_views_to_add  + new_content[end_index_import:]
+                
                 
                 # Faylni qayta yozish
                 new_py_file = Path(self.app_name+'/urls.py')
@@ -224,7 +301,32 @@ class ModelManager:
                 
 
             else:
-                print("urlpatterns pattern not found in the file.")
+                try:
+                    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+                    # Faylni to'liq yo'l (absolute path) bilan topib olamiz
+                    source_file = os.path.join(current_dir, 'model_crud_files/urls.py-tpl')
+
+                    new_text_file = Path(source_file)
+                    content = new_text_file.read_text()
+
+                    replacement_dict = {
+                        '{model_name}': self.model_name,  
+                    }
+                    
+                    for key, value in replacement_dict.items():
+                        content = content.replace(key, value)
+                        
+                    new_py_file = Path(self.app_name+'/urls.py')
+                    new_py_file.write_text(content)
+                    
+                    print(f"urls.py '{self.app_name}' update successfully.")
+                except FileNotFoundError:
+                    print("urls.py fayli topilmadi.")
+                except PermissionError:
+                    print(f"Ruxsat mavjud emas '{self.app_name}' papkasiga nusxalash uchun.")
+                except Exception as e:
+                    print(f"Xatolik yuz berdi: {e}")
     
     def create_urls_file(self):
         try:
